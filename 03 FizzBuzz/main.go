3f783cf167373023d99f960ce2ee785c2290dbfd
infinitejones <git@m3l.io>
2016-12-29 09:07:23 +1100

    Fix FizzBuzz using switch/case/default

--- 03 FizzBuzz/main.go
+++ 03 FizzBuzz/main.go
@@ -5,16 +5,17 @@ import "fmt"
 func main() {
 	for i := 1; i < 101; i++ {
 
-		if i % 3 == 0 {
-			if i % 5 == 0 {
-				fmt.Println(i, ": FizzBuzz")
-			} else {
-				fmt.Println(i, ": Fizz")
-			}
-		}
-
-		if i % 5 == 0 {
-			fmt.Println(i, ": Buzz")
+		switch {
+			case i % 3 == 0:
+				if i % 5 == 0 {
+					fmt.Println(i, "FizzBuzz")
+				} else {
+					fmt.Println(i, "Fizz")
+				}
+			case i % 5 == 0:
+				fmt.Println(i, "Buzz")
+			default:
+				fmt.Println(i)
 		}
 	}
 }
